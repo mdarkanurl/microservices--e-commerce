aba45f3b75adb5203315faecc81487f724081665
mdarkanurl <mdarkanurl@gmail.com>
2025-05-06 21:10:28 -0800

    stuck in inventory POST route

--- src/services/inventory/src/index.ts
+++ src/services/inventory/src/index.ts
@@ -1,8 +1,20 @@
 import express from "express";
 import { config } from "dotenv";
 
+import { createInventory } from "./controllers";
+
 const app = express();
 config();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.post('/inventory', createInventory);
+app.get('/', (req, res) => {
+    res.status(200).send('Hello Postman');
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`http://localhost:${process.env.PORT}`);
